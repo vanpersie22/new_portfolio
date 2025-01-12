6065f50b12adcac498b252d73f2872badb74fbd1
John Doe <ezealionas@gmail.com>
2025-01-12 12:33:30 -0800

    Updated footer and other components

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,38 +1,33 @@
 const Footer = () => {
   return (
     <section
-      className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between 
-  items-center flex-wrap gap-5"
+      className="c-space pt-7 pb-3 border-t border-black-300 flex flex-col sm:flex-row justify-between items-center flex-wrap gap-5 text-center sm:text-left"
     >
-      <div className="text-white-500 flex gap-2">
+      <div className="text-white-500 flex gap-2 justify-center sm:justify-start">
         <p>Terms & Conditions</p>
         <p>|</p>
         <p>Privacy</p>
       </div>
-      <div className="flex gap-3 cursor-pointer">
-        <div className="social-icon ">
-         
-            <img
-              src="/assets/github.svg"
-              alt="github"
-              className="w-1/2 h-1/2"
-            />
-        
+      <div className="flex gap-3 cursor-pointer justify-center sm:justify-start">
+        <div className="social-icon">
+          <img
+            src="/assets/github.svg"
+            alt="github"
+            className="w-6 h-6"
+          />
         </div>
         <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="github" className="w-1/2 h-1/2" />
+          <img src="/assets/twitter.svg" alt="twitter" className="w-6 h-6" />
         </div>
         <div className="social-icon">
-        
-            <img
-              src="/assets/icons8-linkedin.svg"
-              alt="LinkedIn"
-              className="w-1/2 h-1/2"
-            />
-         
+          <img
+            src="/assets/icons8-linkedin.svg"
+            alt="LinkedIn"
+            className="w-6 h-6"
+          />
         </div>
       </div>
-      <p className="text-white-500 ">
+      <p className="text-white-500 w-full sm:w-auto">
         &copy; 2023 CWJ. <span className="text-white-800"></span> All rights reserved.
       </p>
     </section>
